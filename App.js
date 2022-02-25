b4a83591fcd83c139c90745ce3e37e1132550d05
Mohamed A <mohamedammar10@live.fr>
2022-02-25 23:45:22 +0100

    +date randomizer

--- App.js
+++ App.js
@@ -28,22 +28,37 @@ export default class Chat extends Component {
     };
 
 
-    for (var i = 0; i < 20; i++) {
+    for (var i = 0; i < 100; i++) {
+     let m = Math.random() < 0.2 ? Math.random() < 0.1 ? 0 : 2 : 1;
       this.state.messages.push({
         id: Math.floor(Math.random() * 100000000) + 1,
         user: Math.random() < 0.5 ? "Ahmed" : "Mohamed",
-        msg: "hhh hhhhhhhhhh hhhhhhhh feqfzfs zefzr fsfaerg dgqergq ezgqrg sdfqsdf sdfqsdf",
-        type: Math.random() < 0.2 ? Math.random() < 0.1? 0: 2 : 1 ,
-        link: "https://picsum.photos/200/300?random="+Math.floor(Math.random() * 10),
-        status: Math.random() < 0.3 ? Math.random() < 0.5? 0: 2 : 1 ,
-        createdAt: "20-20-2022:10:50:12"
+        msg:m==0? this.sentence(2) : this.sentence(20),
+        type: m ,
+        link: "https://picsum.photos/200/300?random=" + Math.floor(Math.random() * 10),
+        status: Math.random() < 0.3 ? Math.random() < 0.5 ? 0 : 2 : 1,
+        createdAt: new Date((new Date()) - Math.floor(Math.random()*10000000000)).toISOString()
       })
     }
 
 
-
   }
 
+
+   sentence (m) {
+    const words = ["a", "ac", "accumsan", "ad", "adipiscing", "aenean", "aenean", "aliquam", "aliquam", "aliquet", "amet", "ante", "aptent", "arcu", "at", "auctor", "augue", "bibendum", "blandit", "class", "commodo", "condimentum", "congue", "consectetur", "consequat", "conubia", "convallis", "cras", "cubilia", "curabitur", "curabitur", "curae", "cursus", "dapibus", "diam", "dictum", "dictumst", "dolor", "donec", "donec", "dui", "duis", "egestas", "eget", "eleifend", "elementum", "elit", "enim", "erat", "eros", "est", "et", "etiam", "etiam", "eu", "euismod", "facilisis", "fames", "faucibus", "felis", "fermentum", "feugiat", "fringilla", "fusce", "gravida", "habitant", "habitasse", "hac", "hendrerit", "himenaeos", "iaculis", "id", "imperdiet", "in", "inceptos", "integer", "interdum", "ipsum", "justo", "lacinia", "lacus", "laoreet", "lectus", "leo", "libero", "ligula", "litora", "lobortis", "lorem", "luctus", "maecenas", "magna", "malesuada", "massa", "mattis", "mauris", "metus", "mi", "molestie", "mollis", "morbi", "nam", "nec", "neque", "netus", "nibh", "nisi", "nisl", "non", "nostra", "nulla", "nullam", "nunc", "odio", "orci", "ornare", "pellentesque", "per", "pharetra", "phasellus", "placerat", "platea", "porta", "porttitor", "posuere", "potenti", "praesent", "pretium", "primis", "proin", "pulvinar", "purus", "quam", "quis", "quisque", "quisque", "rhoncus", "risus", "rutrum", "sagittis", "sapien", "scelerisque", "sed", "sem", "semper", "senectus", "sit", "sociosqu", "sodales", "sollicitudin", "suscipit", "suspendisse", "taciti", "tellus", "tempor", "tempus", "tincidunt", "torquent", "tortor", "tristique", "turpis", "ullamcorper", "ultrices", "ultricies", "urna", "ut", "ut", "varius", "vehicula", "vel", "velit", "venenatis", "vestibulum", "vitae", "vivamus", "viverra", "volutpat", "vulputate"];
+    const count = Math.round(Math.abs(20) * 1 + m);
+    var result = [];
+    while (result.length < count) {
+      var pos = Math.floor(Math.random() * words.length);
+      var rnd = words[pos];
+      result.push(rnd);
+    }
+    console.log(result);
+    return result.join(' ');
+
+  }
+  
   componentDidUpdate(prevProps = {}) {
     //setTimeout(() => this.scrollToBottom(false), 200);
   }
@@ -51,7 +66,7 @@ export default class Chat extends Component {
 
   send = (msg) => {
 
-    console.log(JSON.stringify (msg));
+    console.log(JSON.stringify(msg));
 
 
   }
