e48bb966c55885531d7e05255fb2f455d29c71fc
Mohamed A <mohamedammar10@live.fr>
2022-03-13 12:57:10 +0100

    Optimizations

--- Components/Input.js
+++ Components/Input.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, Button, TouchableOpacity } from 'react-native';
-import KeyboardAvoidingView from 'react-native/Libraries/Components/Keyboard/KeyboardAvoidingView';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
 export default class Input extends Component {
     constructor(props) {
@@ -12,33 +11,24 @@ export default class Input extends Component {
 
     onSend = () => {
         this.props.onSend(this.state.text)
-        this.setState({ text :""})
+        this.setState({ text: "" })
     };
 
     render() {
         return (
-            <KeyboardAvoidingView behavior="height">
-                <View style={{ flexDirection: "row", alignContent: "space-around", backgroundColor: "#eeeeee", alignItems: "center", height:60, }}>
-                    <TextInput
-                        style={{
-                            flex: 1, backgroundColor: "#fff", color: "#000", borderWidth: 1,
-                            borderColor: "#fff", borderRadius: 30, paddingLeft:15, margin: 8, 
-                        }}
-                        value={this.state.text}
-                        placeholderTextColor="#696969"
-                        onChangeText={text => this.setState({ text })}
-                        blurOnSubmit={false}
-                        placeholder="Type a message"
-                        returnKeyType="send" />
-                    <TouchableOpacity
-                        style={{
-                            marginRight: 5, padding:8,
-                             backgroundColor: "#00BFFF", borderRadius: 15, borderColor: "#fff", borderWidth: 1
-                        }}
-                        onPress={() => this.onSend()}>
-                        <Text style={{ fontSize: 16, color:"#fff", textAlignVertical:"center" }}>Send</Text></TouchableOpacity>
-                </View>
-            </KeyboardAvoidingView>
+            <View style={{ flexDirection: "row", alignContent: "space-around", backgroundColor: "#eeeeee", alignItems: "center", height: 60, }}>
+                <TextInput
+                    style={{ flex: 1, backgroundColor: "#fff", color: "#000", borderWidth: 1, borderColor: "#fff", borderRadius: 30, paddingLeft: 15, margin: 8, }}
+                    value={this.state.text}
+                    placeholderTextColor="#696969"
+                    onChangeText={text => this.setState({ text })}
+                    blurOnSubmit={false}
+                    placeholder="Type a message"
+                    returnKeyType="send" />
+                <TouchableOpacity style={{ marginRight: 5, padding: 8, backgroundColor: "#00BFFF", borderRadius: 15, borderColor: "#fff", borderWidth: 1 }}
+                    onPress={() => this.onSend()}>
+                    <Text style={{ fontSize: 16, color: "#fff", textAlignVertical: "center" }}>Send</Text></TouchableOpacity>
+            </View>
 
         );
     }
